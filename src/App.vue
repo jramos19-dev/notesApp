0148595b86f65b2432e2daa925f492ccecc6862c
jramos19-dev <jramos19@southtexascollege.edu>
2021-04-14 20:20:11 -0500

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <mdi-account class="text-blue-800 h-28 w-28"/>
- <p>
-   Jorge's Notes App
- </p>
-
+ <NavBar/>
 </template>
 
---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <div class="bg-rose-500 ">
+
+
+        <div class="p-4 flex place-items-center space-x-3 text-3xl container mx-auto text-gray-500">
+        <mdi-account />
+        <p class="tracking-wider">
+        Jorge's<span class="font-thin">Notes</span> 
+        </p>
+
+        </div>
+      
+    </div>
+
+</template>
