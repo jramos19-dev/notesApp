773b6c78b686efbe511d10ac91fac255d2f1c01b
jramos19-dev <jramos19@southtexascollege.edu>
2021-04-14 19:50:23 -0500

    initial commit, project setup

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <mdi-account class="text-blue-800 h-28 w-28"/>
+ <p>
+   Jorge's Notes App
+ </p>
+
+</template>
+
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import viteComponents from 'vite-plugin-components'
+import Icons, {ViteIconsResolver} from 'vite-plugin-icons'
+
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': 'src',
+    },
+  },
+
+  plugins: [vue(), viteComponents({
+    customComponentResolvers: ViteIconsResolver({
+    componentPrefix: '',
+  }) }),
+    Icons(),
+  ]
+})
